d1a10d2e8b02ce128fcf9208aa747ff923e08d6d
Kevin <mrkev63@gmail.com>
2024-08-22 18:35:34 -0400

    search is done

--- freeventure/src/App.jsx
+++ freeventure/src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import WeatherContainer from './components/WeatherContainer'
 import NavBar from './components/Navigation'
 import SearchBar from './components/Searchbar'
+import { API_KEY } from '../config'
+import { handleFetch } from '../utils'
 
-const locations = ['new york', 'london', 'paris'];
+// const locations = ['new york', 'london', 'paris'];
 
 function App() {
+  const [weather, setWeather] = useState([])
   const [error, setError] = useState(null)
+
+  const handleSubmit = async (location) => {
+    event.preventDefault();
+    const [data, error] = await handleFetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=yes`)
+    if (data) setWeather(data)
+    if (error) setError(error)
+  }
+
+  if (error) return <p>{error.message}</p>
+
+  useEffect(() => {
+    console.log('TEST:', weather)
+  }, [weather])
 
   return (
     <>
       <NavBar title="Weather Watchers" />
       <WeatherContainer setError={setError} />
       <h2>What's The Weather? </h2>
-      <SearchBar />
+      <SearchBar handleSubmit={handleSubmit} />
 
     </>
   )
--- freeventure/src/components/Searchbar.jsx
+++ freeventure/src/components/Searchbar.jsx
@@ -1,8 +1,18 @@
-const SearchBar = () => {
+import { useState } from "react"
+
+
+const SearchBar = ({ handleSubmit }) => {
+
+  const [value, setValue] = useState('');
+
+  const onSubmit = () => {
+    handleSubmit(value)
+  }
+
   return (
-    <form>
-      <label htmlFor="search-bar">Find it here </label>
-      <input type="text" id="location-search" placeholder="Location" />
+    <form onSubmit={onSubmit}>
+      <label htmlFor="location-search">Find it here </label>
+      <input type="text" id="location-search" value={value} onChange={(e) => setValue(e.target.value)} />
       <button type="submit" class="search-button">Search</button>
     </form>
   )
