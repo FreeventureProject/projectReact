672b06e3beb5fbdc9617075b95675c8b761d6f75
Rashell Aldas <rashellaldas@rashells-mbp.mynetworksettings.com>
2024-08-26 14:17:18 -0400

    notFinished

--- freeventure/src/components/AstonomyContainer.jsx
+++ freeventure/src/components/AstonomyContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { handleFetch } from "../../utils";
 import { API_KEY } from "../../config";
+import SunriseIcon from "../assets/SunriseIcon.png"
 
 // fetching astronomy
 function AstronomyContainer({ astronomyData }) {
@@ -24,7 +25,10 @@ function AstronomyContainer({ astronomyData }) {
       {astronomyData.location && (
         <>
           <h3>{astronomyData.location.name}</h3>
-          <p>{astronomyData.astronomy.astro.sunrise}</p>
+          <div className="sunrise">
+            <p>{astronomyData.astronomy.astro.sunrise}</p>
+            <img id="sun" src={SunriseIcon} alt="Sun" />
+          </div>
           <p>{astronomyData.astronomy.astro.sunset}</p>
         </>
       )}
