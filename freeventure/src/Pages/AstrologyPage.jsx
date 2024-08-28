cefcb3075cd6bf76ea89b8eb7543084b2382c725
Kevin <mrkev63@gmail.com>
2024-08-27 23:27:56 -0400

    small edit

--- freeventure/src/Pages/AstrologyPage.jsx
+++ freeventure/src/Pages/AstrologyPage.jsx
@@ -18,6 +18,7 @@ const AstronomyPage = () => {
   const darkTheme = useTheme()
 
   const themeStyles = {
+    backgroundColor: darkTheme ? '#EBEBEB' : '#1B2222',
     color: darkTheme ? '#455F4E' : '#CCDAD1',
     transition: 'background-color 0.3s, color 0.3s',
   };
